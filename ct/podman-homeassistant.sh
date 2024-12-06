bd12ce4073df601f6561f416c7e570ab5aacec53
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-12-06 10:43:02 +0100

    Update ALL CT's to new default

--- ct/podman-homeassistant.sh
+++ ct/podman-homeassistant.sh
@@ -2,8 +2,7 @@
 source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
-# License: MIT
-# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 
 function header_info {
 clear
@@ -21,40 +20,19 @@ cat <<"EOF"
 EOF
 }
 header_info
-echo -e "Loading..."
 APP="Podman-Home Assistant"
-var_disk="16"
 var_cpu="2"
 var_ram="2048"
+var_disk="16"
 var_os="debian"
 var_version="12"
+var_unprivileged="1"
+base_settings
+
+# Core 
 variables
 color
 catch_errors
-
-function default_settings() {
-  CT_TYPE="1"
-  PW=""
-  CT_ID=$NEXTID
-  HN=$NSAPP
-  DISK_SIZE="$var_disk"
-  CORE_COUNT="$var_cpu"
-  RAM_SIZE="$var_ram"
-  BRG="vmbr0"
-  NET="dhcp"
-  GATE=""
-  APT_CACHER=""
-  APT_CACHER_IP=""
-  DISABLEIP6="no"
-  MTU=""
-  SD=""
-  NS=""
-  MAC=""
-  VLAN=""
-  SSH="no"
-  VERB="no"
-  echo_default
-}
 
 function update_script() {
 header_info
@@ -142,5 +120,6 @@ build_container
 description
 
 msg_ok "Completed Successfully!\n"
-echo -e "${APP} should be reachable by going to the following URL.
-         ${BL}http://${IP}:8123${CL} \n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8123${CL}"
